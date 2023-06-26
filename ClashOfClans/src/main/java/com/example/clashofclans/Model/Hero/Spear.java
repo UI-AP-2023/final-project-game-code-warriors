49e244535f24d4373dda757e56e0ddc58e51baeb
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-06-27 00:32:46 +0330

    Spear model and images added

---
+++ ClashOfClans/src/main/java/com/example/clashofclans/Model/Hero/Spear.java
@@ -0,0 +1,9 @@
+package com.example.clashofclans.Model.Hero;
+
+import com.example.clashofclans.Values;
+
+public class Spear extends Hero{
+    public Spear() {
+        super(Values.SPEAR_HEALTH,Values.SPEAR_HIT,Values.SPEAR_SPEED);
+    }
+}
--- ClashOfClans/src/main/java/com/example/clashofclans/Values.java
+++ ClashOfClans/src/main/java/com/example/clashofclans/Values.java
@@ -3,12 +3,19 @@ package com.example.clashofclans;
 public class Values {
 
     // Max health = 100; Max hit/second = 50; Max speed = 20;
+
+    // Knight - middle
     public static int KNIGHT_HEALTH = 70;
     public static int KNIGHT_HIT = 20;
     public static int KNIGHT_SPEED = 10;
 
-
+    // Green - weak
     public static int GREEN_HEALTH = 45;
-    public static int GREEN_HIT = 10;
-    public static int GREEN_SPEED = 30;
+    public static int GREEN_HIT = 15;
+    public static int GREEN_SPEED = 40;
+
+    // Spear - middle
+    public static int SPEAR_HEALTH = 80;
+    public static int SPEAR_HIT = 15;
+    public static int SPEAR_SPEED = 15;
 }
