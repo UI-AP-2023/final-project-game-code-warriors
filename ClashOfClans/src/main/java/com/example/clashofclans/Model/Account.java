0ca0d6054cc1506857e212003ea6a3013b4574e6
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-06-28 12:41:29 +0330

    Account class added

---
+++ ClashOfClans/src/main/java/com/example/clashofclans/Model/Account.java
@@ -0,0 +1,13 @@
+package com.example.clashofclans.Model;
+
+import java.util.SplittableRandom;
+
+public class Account {
+    private String username;
+    private String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
