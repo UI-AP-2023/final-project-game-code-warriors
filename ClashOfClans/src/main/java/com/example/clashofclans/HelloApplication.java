aac058efdcf5695420b6d3105a2bea66b97d648f
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-06-30 21:55:29 +0330

    Buttons added to main page

--- ClashOfClans/src/main/java/com/example/clashofclans/HelloApplication.java
+++ ClashOfClans/src/main/java/com/example/clashofclans/HelloApplication.java
@@ -1,9 +1,14 @@
 package com.example.clashofclans;
 
 import com.example.clashofclans.Utility.MapHandler;
+import com.example.clashofclans.Widgets.HomeButton;
+import com.example.clashofclans.Widgets.MenuButton;
+import com.example.clashofclans.Widgets.SettingButton;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +16,25 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        ScrollPane root = new ScrollPane(Values.maps.get(1));
+        ScrollPane map = new ScrollPane(Values.maps.get(1));
+        map.setPannable(true);
+        map.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        map.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        MenuButton menuButton = new MenuButton(30);
+        menuButton.setTranslateX(-470);
+        menuButton.setTranslateY(-270);
+        menuButton.setOnMouseClicked(mouseEvent -> {
+            VBox vBox_Menu = new VBox();
+
+        });
+
+        SettingButton settingButton = new SettingButton(30);
+        settingButton.setTranslateX(-435);
+        settingButton.setTranslateY(-270);
+
+        StackPane root = new StackPane(map, menuButton,settingButton);
+        root.setPrefSize(1000, 600);
 
         Scene scene = new Scene(root, 1000, 600);
 
