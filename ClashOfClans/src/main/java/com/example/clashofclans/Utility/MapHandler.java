8a749969c37e411d36910d778547a738bd54de79
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-07-02 21:37:45 +0330

    refactor map handler

--- ClashOfClans/src/main/java/com/example/clashofclans/Utility/MapHandler.java
+++ ClashOfClans/src/main/java/com/example/clashofclans/Utility/MapHandler.java
@@ -9,41 +9,41 @@ import javafx.scene.layout.AnchorPane;
 
 public class MapHandler {
     public static void map1() {
-        HomeLevel3 home1 = new HomeLevel3();
+        HomeLevel3 home1 = new HomeLevel3(60);
         home1.setInsets(280, 530);
 
-        HomeLevel2 home2 = new HomeLevel2();
+        HomeLevel2 home2 = new HomeLevel2(60);
         home2.setInsets(300, 500);
 
-        HomeLevel2 home3 = new HomeLevel2();
+        HomeLevel2 home3 = new HomeLevel2(60);
         home3.setInsets(320, 470);
 
-        HomeLevel2 home4 = new HomeLevel2();
+        HomeLevel2 home4 = new HomeLevel2(60);
         home4.setInsets(340, 440);
 
-        Storage storage1 = new Storage();
+        Storage storage1 = new Storage(80);
         storage1.setInsets(450, 300);
 
-        Storage storage2 = new Storage();
+        Storage storage2 = new Storage(80);
         storage2.setInsets(475, 335);
 
-        Storage storage3 = new Storage();
+        Storage storage3 = new Storage(80);
         storage3.setInsets(500, 370);
 
-        Canon canon1 = new Canon();
+        Canon canon1 = new Canon(60);
         canon1.setInsets(350, 550);
 
-        Canon canon2 = new Canon();
+        Canon canon2 = new Canon(60);
         canon2.setInsets(400, 900);
 
-        Thrower thrower1 = new Thrower();
+        Thrower thrower1 = new Thrower(80);
         thrower1.setInsets(130, 500);
 
-        Barracks barracks1 = new Barracks();
+        Barracks barracks1 = new Barracks(60);
         barracks1.setInsets(250, 700);
 
         Field field = new Field();
-        field.getChildren().addAll(home1.getImageView(60), home2.getImageView(60), home3.getImageView(60), home4.getImageView(60), storage1.getImageView(80), storage2.getImageView(80), storage3.getImageView(80), canon1.getImageView(60), canon2.getImageView(60), thrower1.getImageView(80), barracks1.getImageView(60));
+        field.addBulkChildren(home1 , home2 , home3 , home4 , storage1 , storage2 ,storage3 , canon1 , canon2 ,thrower1 , barracks1);
 
         ScrollPane scrollPane = new ScrollPane(field);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
@@ -54,45 +54,44 @@ public class MapHandler {
     }
 
     public static void map2() {
-        HomeLevel2 home1 = new HomeLevel2();
+        HomeLevel2 home1 = new HomeLevel2(70);
         home1.setInsets(350, 550);
 
-        Barracks barracks1 = new Barracks();
+        Barracks barracks1 = new Barracks(60);
         barracks1.setInsets(380, 700);
 
-        Barracks barracks2 = new Barracks();
+        Barracks barracks2 = new Barracks(60);
         barracks2.setInsets(500, 550);
 
-        Storage storage1 = new Storage();
+        Storage storage1 = new Storage(70);
         storage1.setInsets(350, 430);
 
-        Thrower thrower1 = new Thrower();
+        Thrower thrower1 = new Thrower(60);
         thrower1.setInsets(430, 550);
 
-        Thrower thrower2 = new Thrower();
+        Thrower thrower2 = new Thrower(60);
         thrower2.setInsets(260, 550);
 
-        Canon canon1 = new Canon();
+        Canon canon1 = new Canon(50);
         canon1.setInsets(280, 450);
 
-        Canon canon2 = new Canon();
+        Canon canon2 = new Canon(50);
         canon2.setInsets(500, 700);
 
-        Castle castle = new Castle();
+        Castle castle = new Castle(60);
         castle.setInsets(250,700);
 
-        HomeLevel2 home2 = new HomeLevel2();
+        HomeLevel2 home2 = new HomeLevel2(70);
         home2.setInsets(450,210);
 
-        HomeLevel2 home3 = new HomeLevel2();
+        HomeLevel2 home3 = new HomeLevel2(70);
         home3.setInsets(480,255);
 
-        HomeLevel2 home4 = new HomeLevel2();
+        HomeLevel2 home4 = new HomeLevel2(70);
         home4.setInsets(650,600);
 
         Field field = new Field();
-        field.getChildren().addAll(home1.getImageView(70), barracks1.getImageView(60), barracks2.getImageView(60), storage1.getImageView(70), thrower1.getImageView(60), thrower2.getImageView(60), canon1.getImageView(50), canon2.getImageView(50), castle.getImageView(60),home2.getImageView(70),home3.getImageView(70),home4.getImageView(70));
-
+        field.addBulkChildren(home1, barracks1, barracks2, storage1, thrower1, thrower2, canon1, canon2, castle, home2, home3, home4);
         ScrollPane scrollPane = new ScrollPane(field);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
